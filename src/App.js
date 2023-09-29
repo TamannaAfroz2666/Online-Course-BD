31cf3ac5db946d50ff82173c6b448f3aae82285e
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-29 22:48:03 +0600

    add file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-// import logo from './logo.svg';
+
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div >
-      <h1>hhhh</h1>
+      <Home></Home>
+ 
     
     </div>
   );
