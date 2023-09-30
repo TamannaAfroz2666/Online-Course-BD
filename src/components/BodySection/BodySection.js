52b026ca0af267d6b651d93fdd4dd150b623972b
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-30 15:50:17 +0600

    Body structure created

--- src/components/BodySection/BodySection.js
+++ src/components/BodySection/BodySection.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import './BodySection.css';
 
 const BodySection = () => {
     return (
         <div>
-            <h1>hh</h1>
+           <div className="backgroundPart">
+            <h1>Education & Training  Organization</h1>
+            <p>We provide the best Education System for you</p>
+
+            <div className="btnAll">
+                <div className="forAllClass">
+                    <a href="">
+                    <button>Our Course</button>
+                    </a>
+                    
+
+                </div>
+                <div className="btnContactUs">
+                    <a href="">
+                        <button>Contact Us</button>
+                    </a>
+
+                </div>
+            </div>
+
+           </div>
         </div>
     );
 };
