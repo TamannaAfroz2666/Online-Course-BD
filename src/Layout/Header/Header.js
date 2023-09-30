784af3aa8063a14d83c59bd18697b507a79a5402
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-30 12:32:29 +0600

    divided

--- src/Layout/Header/Header.js
+++ src/Layout/Header/Header.js
@@ -8,17 +8,27 @@ const Header = () => {
         <div>
             <div className="headerMain">
                 <div className="headerSub">
-                    <div className="logo"> 
-                    <a href="#">
-                       <img src={logo} alt="wait please ....." />
-                       <p>Edu <span>Zone</span> <br /><small>Happy Education</small></p>
-                       </a>
+                    <div className="logo">
+                        <a href="#">
+                            <img src={logo} alt="wait please ....." />
+                            <p>Edu <span>Zone</span> <br /><small>Happy Education</small></p>
+                        </a>
+                    </div>
+
+                    <div className="navbarMain">
+                        <a href="#">Home</a>
+                        <a href="#">About</a>
+                        <a href="#">Course</a>
+                        <a href="#">Lesson</a>
+                        <a href="#">Pages</a>
+                        <a href="#">Blog</a>
+                        <a href="#">Contact</a>
                     </div>
 
                 </div>
             </div>
-         
-            
+
+
         </div>
     );
 };
