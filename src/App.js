fcb281414acd3aaab5c0e5657845f3639a0512ce
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-30 15:12:46 +0600

    try to connect for static

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Header from './Layout/Header/Header';
 import TopHeader from './Layout/TopHeader/TopHeader';
+import Home from './components/Home/Home';
 
 
 
@@ -10,6 +11,7 @@ function App() {
     <div className='container'>
       <TopHeader></TopHeader>
       <Header></Header>
+      <Home></Home>
  
     
     </div>
