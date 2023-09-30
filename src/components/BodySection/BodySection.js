6bc79a2d4256d02dbe16ed98c29457d238a95e78
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-30 16:17:26 +0600

    linier gradient used on background image

--- src/components/BodySection/BodySection.js
+++ src/components/BodySection/BodySection.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import './BodySection.css';
 
+
 const BodySection = () => {
     return (
-        <div>
-           <div className="backgroundPart">
-            <h1>Education & Training  Organization</h1>
-            <p>We provide the best Education System for you</p>
+        <div className='BodySection'>
+            <div className="backgroundPart">
 
-            <div className="btnAll">
-                <div className="forAllClass">
-                    <a href="">
-                    <button>Our Course</button>
-                    </a>
-                    
+                <h1>Education & Training  Organization</h1>
+                <p>We provide the best Education System for you</p>
 
+                <div className="btnAll">
+                    <div className="forAllClass">
+                        <a href="#">
+                            <button>Our Course</button>
+                        </a>
+
+
+                    </div>
+                    <div className="btnContactUs">
+                        <a href="#">
+                            <button>Contact Us</button>
+                        </a>
+
+                    </div>
                 </div>
-                <div className="btnContactUs">
-                    <a href="">
-                        <button>Contact Us</button>
-                    </a>
 
-                </div>
             </div>
-
-           </div>
         </div>
     );
 };
