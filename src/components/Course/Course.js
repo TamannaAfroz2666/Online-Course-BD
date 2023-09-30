8ad385bcf72e5fb2d65249e61afeb698114af46e
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-30 22:00:10 +0600

    horizontal designed

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -6,7 +6,18 @@ const Course = () => {
         <div className='courseSection'>
             <div className="courses">
                 <h1>Our Popular Courses</h1>
+                <div className="lineDirection">
+                    <span className='horizontal'>
+                        <small className='horizontalSmall'> <hr color="#f04b4b"
+                            size="4" /></small>
+                        <hr color="#f04b4b"
+                            size="4" />
+                        <small><hr color="#f04b4b"
+                            size="4" /></small>
+                    </span>
+                </div>
             </div>
+            <h1 className='helo'> hello</h1>
         </div>
     );
 };
