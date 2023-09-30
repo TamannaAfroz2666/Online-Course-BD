bcd463e0fd9a302f713c40105fc8929ea0464727
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-30 21:27:45 +0600

    Course component create

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Home.css'
 import BodySection from '../BodySection/BodySection';
+import Course from '../Course/Course';
 
 const Home = () => {
     return (
         <div>
             <BodySection></BodySection>
+            <Course></Course>
           
         </div>
     );
