5b26b8c3a9987e8de5d2b733f095582b201d529e
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-01 15:20:08 +0600

    add styled

--- src/components/Course/ParentCard/ParentCard.js
+++ src/components/Course/ParentCard/ParentCard.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import './ParentCard.css';
+import Card from '../Card/Card';
+import DigitalMarketing from '../Card/DigitalMarketing/DigitalMarketing';
+import WebDevelopment from '../Card/WebDevelopment/WebDevelopment';
 
 const ParentCard = () => {
     return (
-        <div>
+        <div className='parentCard'>
+            <div className="header">
+                <div className="business">
+                    <Card></Card>
+                </div>
+                <div className="digitalMarketing">
+                    <DigitalMarketing></DigitalMarketing>
+                </div>
+                <div className="webDevelopment">
+                    <WebDevelopment></WebDevelopment>
+                </div>
+            </div>
             
         </div>
     );
