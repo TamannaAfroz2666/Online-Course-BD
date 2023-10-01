8de9f8eca828fdde54708ffc5281e5cf147f0e6c
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-01 12:31:05 +0600

    sub component are added

---
+++ src/components/Course/Card/DigitalMarketing/DigitalMarketing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './DigitalMarketing.css';
+
+const DigitalMarketing = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default DigitalMarketing;
---
+++ src/components/Course/Card/WebDevelopment/WebDevelopment.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './WebDevelopment.css';
+
+const WebDevelopment = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default WebDevelopment;
