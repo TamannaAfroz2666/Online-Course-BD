8e4fd9b4a5c34804ab352f61ee8d08776854f547
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-01 23:24:57 +0600

    title designed

--- src/components/ChooseUs/ChooseInfo/ChooseInfo.js
+++ src/components/ChooseUs/ChooseInfo/ChooseInfo.js
@@ -8,22 +8,23 @@ const ChooseInfo = () => {
             <div className="infoHead">
                 <h1>Why Choose Us</h1>
                 <div className="horizontal">
-                    <hr color='orange' size='4' className='first'/>
-                     <hr color='orange' size='4' className='second' /> 
-                     <hr color='orange' size='4'  className='first'/>
+                    <hr color='orange' size='4' className='first' />
+                    <hr color='orange' size='4' className='second' />
+                    <hr color='orange' size='4' className='first' />
                 </div>
                 <p>Our learning a good option for students who need to balance their work and family commitments.</p>
 
                 <div className="optionsHead">
                     <div className="iconIs">
-                        <IoBookSharp  className='bookIcon'/>
+                        <IoBookSharp className='bookIcon' />
 
                     </div>
                     <div className="shortDescription">
-
+                        <h5>Best Education System</h5>
+                        <span>Advanced Education Management</span>
                     </div>
 
-            
+
                 </div>
 
             </div>
