6c9a5bcdea694a18135eebcebbc3e529bcc970eb
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-02 14:47:02 +0600

    add new component of quantity in cousr

---
+++ src/components/CourseQunantityInfo/CourseQuantityInfo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CourseQuantityInfo = () => {
+    return (
+        <div>
+            <div className="quantityHead">
+                
+            </div>
+            
+        </div>
+    );
+};
+
+export default CourseQuantityInfo;
