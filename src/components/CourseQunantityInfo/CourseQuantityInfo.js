38b1737a3bb0330418da9eb552f957181d757274
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-02 15:28:22 +0600

    based create

--- src/components/CourseQunantityInfo/CourseQuantityInfo.js
+++ src/components/CourseQunantityInfo/CourseQuantityInfo.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import './CourseQuantityInfo.css'
+import './CourseQuantityInfo.css';
+import { AiOutlinePlus } from "react-icons/ai";
 
 const CourseQuantityInfo = () => {
     return (
         <div className='courseQuantity'>
             <div className="quantityHead">
+                <div className="passedGraduates">
+                    <h2>3235</h2>
+                    <span>Passed Graduates</span>
+
+                </div>
+                <div className="coursePublished">
+                    <h2>256 <span> <AiOutlinePlus /> </span></h2>
+                    <span>Passed Graduates</span>
+
+                </div>
+                <div className="qualifiedStaff">
+                    <h2>3235</h2>
+                    <span>Passed Graduates</span>
+
+                </div>
+                <div className="countryReached">
+                    <h2>3235</h2>
+                    <span>Passed Graduates</span>
+
+                </div>
 
             </div>
-            
+
         </div>
     );
 };
