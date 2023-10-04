ce7790fcf5e890f4966e08ad1f5371b7571be3f1
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-04 21:20:29 +0600

    base complete

--- src/components/EventInfo/EventInfo.js
+++ src/components/EventInfo/EventInfo.js
@@ -3,6 +3,7 @@ import './EventInfo.css';
 import { LuClock9 } from "react-icons/lu";
 import { IoLocationSharp } from "react-icons/io5";
 import image1 from '../../Assets/Image/EventPic/Book.jpg'
+// import EventInfoTwo from './EventInfoTwo/EventInfoTwo';
 
 const EventInfo = () => {
     return (
@@ -27,13 +28,13 @@ const EventInfo = () => {
                                     <p><span> <LuClock9 size={14} /> </span>8:00 am - 5:00 pm</p>
                                 </div>
                                 <div className="buildingAddress">
-                                    <p><span> <IoLocationSharp  size={14}/> </span> London city, wclb uk </p>
+                                    <p><span> <IoLocationSharp size={14} /> </span> London city, wclb uk </p>
                                 </div>
                             </div>
                             <div className="btnRegister">
-                            <button type='button'>Register</button>
+                                <button type='button'>Register</button>
                             </div>
-                           
+
                         </div>
 
                     </div>
@@ -47,7 +48,78 @@ const EventInfo = () => {
                     <span> <hr size={2} /> </span>
                 </div>
             </div>
-         
+                   {/*=== second part === */}
+            <div className="eventDetails">
+                <div className="upcomingEvent">
+                    <div className="calender">
+                        <p className='date'>02</p>
+                        <p>March</p>
+                    </div>
+                    <div className="addressInfo">
+                        <h2>Call for applications for the 2018 Letten Prize</h2>
+                        <div className="addressHead">
+                            <div className="timing">
+                                <p><span> <LuClock9 size={14} /> </span>8:00 am - 5:00 pm</p>
+                            </div>
+                            <div className="buildingAddress">
+                                <p><span> <IoLocationSharp size={14} /> </span> London city, wclb uk </p>
+                            </div>
+                        </div>
+                        <div className="btnRegister">
+                            <button type='button'>Register</button>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div className="eventImage">
+                    <img src={image1} alt="" />
+
+                </div>
+
+            </div>
+            <div className="lineHorizontal">
+                <span> <hr size={2} /> </span>
+            </div>
+
+            {/*=== third part === */}
+
+            <div className="eventDetails">
+                <div className="upcomingEvent">
+                    <div className="calender">
+                        <p className='date'>02</p>
+                        <p>March</p>
+                    </div>
+                    <div className="addressInfo">
+                        <h2>Call for applications for the 2018 Letten Prize</h2>
+                        <div className="addressHead">
+                            <div className="timing">
+                                <p><span> <LuClock9 size={14} /> </span>8:00 am - 5:00 pm</p>
+                            </div>
+                            <div className="buildingAddress">
+                                <p><span> <IoLocationSharp size={14} /> </span> London city, wclb uk </p>
+                            </div>
+                        </div>
+                        <div className="btnRegister">
+                            <button type='button'>Register</button>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div className="eventImage">
+                    <img src={image1} alt="" />
+
+                </div>
+
+            </div>
+            <div className="lineHorizontal">
+                <span> <hr size={2} /> </span>
+            </div>
+
+        
+
+            {/* <EventInfoTwo></EventInfoTwo> */}
 
         </div>
     );
--- src/components/EventInfo/EventInfoTwo/EventInfoTwo.js
+++ src/components/EventInfo/EventInfoTwo/EventInfoTwo.js
@@ -1,53 +1,52 @@
 import React from 'react';
 import './EventInfoTwo.css';
+import { LuClock9 } from "react-icons/lu";
+import { IoLocationSharp } from "react-icons/io5";
+// import image1 from '../../Assets/Image/EventPic/Book.jpg'
+// import EventInfoTwo from './EventInfoTwo/EventInfoTwo';
+import image from '../../../Assets/Image/EventPic/SingleTeach.jpg'
 
 const EventInfoTwo = () => {
     return (
         <div>
-               <div className='eventInfo'>
-            <div className="eventInfoMain">
-                <h1>Upcoming Event</h1>
-                <div className="horizontalEventInfo">
-                    <hr color='orange' size='4' className='firstInfo' />
-                    <hr color='orange' size='4' className='secondInfo' />
-                    <hr color='orange' size='4' className='firstInfo' />
-                </div>
-                <div className="eventDetails">
-                    <div className="upcomingEvent">
-                        <div className="calender">
-                            <p className='date'>02</p>
-                            <p>March</p>
+            <div className='eventInfo'>
+                <div className="eventInfoMain">
+                    <div className="eventDetails">
+                        <div className="upcomingEvent">
+                            <div className="calender">
+                                <p className='date'>02</p>
+                                <p>March</p>
+                            </div>
+                            <div className="addressInfo">
+                                <h2>Call for applications for the 2018 Letten Prize</h2>
+                                <div className="addressHead">
+                                    <div className="timing">
+                                        <p><span> <LuClock9 size={14} /> </span>8:00 am - 5:00 pm</p>
+                                    </div>
+                                    <div className="buildingAddress">
+                                        <p><span> <IoLocationSharp size={14} /> </span> London city, wclb uk </p>
+                                    </div>
+                                </div>
+                                <div className="btnRegister">
+                                    <button type='button'>Register</button>
+                                </div>
+
+                            </div>
+
                         </div>
-                        <div className="addressInfo">
-                            <h2>Call for applications for the 2018 Letten Prize</h2>
-                            <div className="addressHead">
-                                <div className="timing">
-                                    <p><span> <LuClock9 size={14} /> </span>8:00 am - 5:00 pm</p>
-                                </div>
-                                <div className="buildingAddress">
-                                    <p><span> <IoLocationSharp  size={14}/> </span> London city, wclb uk </p>
-                                </div>
-                            </div>
-                            <div className="btnRegister">
-                            <button type='button'>Register</button>
-                            </div>
-                           
+                        <div className="eventImage">
+                            <img src={image} alt="" />
+
                         </div>
 
                     </div>
-                    <div className="eventImage">
-                        <img src={image1} alt="" />
-
+                    <div className="lineHorizontal">
+                        <span> <hr size={2} /> </span>
                     </div>
+                </div>
 
-                </div>
-                <div className="lineHorizontal">
-                    <span> <hr size={2} /> </span>
-                </div>
+
             </div>
-         
-
-        </div>
         </div>
     );
 };
