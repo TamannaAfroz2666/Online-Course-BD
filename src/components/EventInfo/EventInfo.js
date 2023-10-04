c85e46af9cf44a2f52f84ac1df215b4689665509
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-04 21:31:02 +0600

    add component created

--- src/components/EventInfo/EventInfo.js
+++ src/components/EventInfo/EventInfo.js
@@ -4,6 +4,7 @@ import { LuClock9 } from "react-icons/lu";
 import { IoLocationSharp } from "react-icons/io5";
 import image1 from '../../Assets/Image/EventPic/Book.jpg'
 import EventInfoTwo from './EventInfoTwo/EventInfoTwo';
+import EventInfoLast from './EventInfoLast/EventInfoLast';
 
 const EventInfo = () => {
     return (
@@ -48,42 +49,11 @@ const EventInfo = () => {
                     <span> <hr size={2} /> </span>
                 </div>
             </div>
-                   {/*=== second part === */}
-            <div className="eventDetails">
-                <div className="upcomingEvent">
-                    <div className="calender">
-                        <p className='date'>02</p>
-                        <p>March</p>
-                    </div>
-                    <div className="addressInfo">
-                        <h2>Call for applications for the 2018 Letten Prize</h2>
-                        <div className="addressHead">
-                            <div className="timing">
-                                <p><span> <LuClock9 size={14} /> </span>8:00 am - 5:00 pm</p>
-                            </div>
-                            <div className="buildingAddress">
-                                <p><span> <IoLocationSharp size={14} /> </span> London city, wclb uk </p>
-                            </div>
-                        </div>
-                        <div className="btnRegister">
-                            <button type='button'>Register</button>
-                        </div>
-
-                    </div>
-
-                </div>
-                <div className="eventImage">
-                    <img src={image1} alt="" />
-
-                </div>
-
-            </div>
-            <div className="lineHorizontal">
-                <span> <hr size={2} /> </span>
-            </div>
+            {/*=== second part === */}
+            <EventInfoTwo></EventInfoTwo>
 
             {/*=== third part === */}
-            <EventInfoTwo></EventInfoTwo>
+            <EventInfoLast></EventInfoLast>
 
         </div>
     );
--- src/components/EventInfo/EventInfoLast/EventInfoLast.js
+++ src/components/EventInfo/EventInfoLast/EventInfoLast.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import './EventInfoLast.css';
+import groupImage from '../../../Assets/Image/EventPic/groupTeach.jpg';
+import { LuClock9 } from "react-icons/lu";
+import { IoLocationSharp } from "react-icons/io5";
 
 const EventInfoLast = () => {
     return (
-        <div>
-            
+        <>
+        <div className="eventDetails">
+            <div className="upcomingEvent">
+                <div className="calender">
+                    <p className='date'>02</p>
+                    <p>March</p>
+                </div>
+                <div className="addressInfo">
+                    <h2>Call for applications for the 2018 Letten Prize</h2>
+                    <div className="addressHead">
+                        <div className="timing">
+                            <p><span> <LuClock9 size={14} /> </span>8:00 am - 5:00 pm</p>
+                        </div>
+                        <div className="buildingAddress">
+                            <p><span> <IoLocationSharp size={14} /> </span> London city, wclb uk </p>
+                        </div>
+                    </div>
+                    <div className="btnRegister">
+                        <button type='button'>Register</button>
+                    </div>
+
+                </div>
+
+            </div>
+            <div className="eventImage">
+                <img src={groupImage} alt="" />
+
+            </div>
+
         </div>
-        
+        <div className="lineHorizontal">
+            <span> <hr size={2} /> </span>
+        </div>
+    </>
+
     );
 };
 
