136dd9848888b461034720328990e779a143cf18
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-04 21:26:42 +0600

    add new page to load data

--- src/components/EventInfo/EventInfo.js
+++ src/components/EventInfo/EventInfo.js
@@ -3,7 +3,7 @@ import './EventInfo.css';
 import { LuClock9 } from "react-icons/lu";
 import { IoLocationSharp } from "react-icons/io5";
 import image1 from '../../Assets/Image/EventPic/Book.jpg'
-// import EventInfoTwo from './EventInfoTwo/EventInfoTwo';
+import EventInfoTwo from './EventInfoTwo/EventInfoTwo';
 
 const EventInfo = () => {
     return (
@@ -83,43 +83,7 @@ const EventInfo = () => {
             </div>
 
             {/*=== third part === */}
-
-            <div className="eventDetails">
-                <div className="upcomingEvent">
-                    <div className="calender">
-                        <p className='date'>02</p>
-                        <p>March</p>
-                    </div>
-                    <div className="addressInfo">
-                        <h2>Call for applications for the 2018 Letten Prize</h2>
-                        <div className="addressHead">
-                            <div className="timing">
-                                <p><span> <LuClock9 size={14} /> </span>8:00 am - 5:00 pm</p>
-                            </div>
-                            <div className="buildingAddress">
-                                <p><span> <IoLocationSharp size={14} /> </span> London city, wclb uk </p>
-                            </div>
-                        </div>
-                        <div className="btnRegister">
-                            <button type='button'>Register</button>
-                        </div>
-
-                    </div>
-
-                </div>
-                <div className="eventImage">
-                    <img src={image1} alt="" />
-
-                </div>
-
-            </div>
-            <div className="lineHorizontal">
-                <span> <hr size={2} /> </span>
-            </div>
-
-        
-
-            {/* <EventInfoTwo></EventInfoTwo> */}
+            <EventInfoTwo></EventInfoTwo>
 
         </div>
     );
--- src/components/EventInfo/EventInfoTwo/EventInfoTwo.js
+++ src/components/EventInfo/EventInfoTwo/EventInfoTwo.js
@@ -2,52 +2,46 @@ import React from 'react';
 import './EventInfoTwo.css';
 import { LuClock9 } from "react-icons/lu";
 import { IoLocationSharp } from "react-icons/io5";
-// import image1 from '../../Assets/Image/EventPic/Book.jpg'
+// import image from '../../Assets/Image/EventPic/Book.jpg'
 // import EventInfoTwo from './EventInfoTwo/EventInfoTwo';
 import image from '../../../Assets/Image/EventPic/SingleTeach.jpg'
 
 const EventInfoTwo = () => {
     return (
-        <div>
-            <div className='eventInfo'>
-                <div className="eventInfoMain">
-                    <div className="eventDetails">
-                        <div className="upcomingEvent">
-                            <div className="calender">
-                                <p className='date'>02</p>
-                                <p>March</p>
+        <>
+            <div className="eventDetails">
+                <div className="upcomingEvent">
+                    <div className="calender">
+                        <p className='date'>02</p>
+                        <p>March</p>
+                    </div>
+                    <div className="addressInfo">
+                        <h2>Call for applications for the 2018 Letten Prize</h2>
+                        <div className="addressHead">
+                            <div className="timing">
+                                <p><span> <LuClock9 size={14} /> </span>8:00 am - 5:00 pm</p>
                             </div>
-                            <div className="addressInfo">
-                                <h2>Call for applications for the 2018 Letten Prize</h2>
-                                <div className="addressHead">
-                                    <div className="timing">
-                                        <p><span> <LuClock9 size={14} /> </span>8:00 am - 5:00 pm</p>
-                                    </div>
-                                    <div className="buildingAddress">
-                                        <p><span> <IoLocationSharp size={14} /> </span> London city, wclb uk </p>
-                                    </div>
-                                </div>
-                                <div className="btnRegister">
-                                    <button type='button'>Register</button>
-                                </div>
-
+                            <div className="buildingAddress">
+                                <p><span> <IoLocationSharp size={14} /> </span> London city, wclb uk </p>
                             </div>
-
                         </div>
-                        <div className="eventImage">
-                            <img src={image} alt="" />
-
+                        <div className="btnRegister">
+                            <button type='button'>Register</button>
                         </div>
 
                     </div>
-                    <div className="lineHorizontal">
-                        <span> <hr size={2} /> </span>
-                    </div>
+
+                </div>
+                <div className="eventImage">
+                    <img src={image} alt="" />
+
                 </div>
 
-
             </div>
-        </div>
+            <div className="lineHorizontal">
+                <span> <hr size={2} /> </span>
+            </div>
+        </>
     );
 };
 
