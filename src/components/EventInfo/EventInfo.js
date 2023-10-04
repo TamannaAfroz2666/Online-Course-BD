8e842f61bfc27861a5774de2e56f58392e037d4b
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-04 10:32:57 +0600

    add some pic

--- src/components/EventInfo/EventInfo.js
+++ src/components/EventInfo/EventInfo.js
@@ -13,26 +13,33 @@ const EventInfo = () => {
                     <hr color='orange' size='4' className='secondInfo' />
                     <hr color='orange' size='4' className='firstInfo' />
                 </div>
+                <div className="eventDetails">
+                    <div className="upcomingEvent">
+                        <div className="calender">
+                            <p className='date'>02</p>
+                            <p>March</p>
 
-                <div className="upcomingEvent">
-                    <div className="calender">
-                        <p className='date'>02</p>
-                        <p>March</p>
-
-                    </div>
-                    <div className="addressInfo">
-                        <h2>Call for applications for the 2018 Letten Prize</h2>
-                        <div className="addressHead">
-                            <div className="timing">
-                                <p><span> <LuClock9 /> </span>8:00 am - 5:00 pm</p>
-                            </div>
-                            <div className="buildingAddress">
-                                <p><span> <IoLocationSharp /> </span> London city, wclb uk </p>
+                        </div>
+                        <div className="addressInfo">
+                            <h2>Call for applications for the 2018 Letten Prize</h2>
+                            <div className="addressHead">
+                                <div className="timing">
+                                    <p><span> <LuClock9 /> </span>8:00 am - 5:00 pm</p>
+                                </div>
+                                <div className="buildingAddress">
+                                    <p><span> <IoLocationSharp /> </span> London city, wclb uk </p>
+                                </div>
                             </div>
                         </div>
+
+                    </div>
+                    <div className="eventImage">
+                        
                     </div>
 
                 </div>
+
+
             </div>
 
         </div>
