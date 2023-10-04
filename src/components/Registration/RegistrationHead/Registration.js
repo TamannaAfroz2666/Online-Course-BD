0947752df4eb4f50625bf6722362b5ba54cb5ab5
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-04 22:03:43 +0600

    registration page base create

--- src/components/Registration/RegistrationHead/Registration.js
+++ src/components/Registration/RegistrationHead/Registration.js
@@ -4,7 +4,16 @@ import './Registration.css';
 const Registration = () => {
     return (
         <div className='registration'>
-            <h1>hello</h1>
+            <div className="registrationHead">
+                <div className="registrationPage">
+                <h1>hello</h1>
+
+                </div>
+                <div className="registrationImage">
+                    
+                </div>
+            </div>
+            
             
         </div>
     );
