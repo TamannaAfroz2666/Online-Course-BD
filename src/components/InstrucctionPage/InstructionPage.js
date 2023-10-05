e78b703cd9199f6878f89ae13bb560d5e8141c5a
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-05 10:02:49 +0600

    Instruction descripe create and designed

--- src/components/InstrucctionPage/InstructionPage.js
+++ src/components/InstrucctionPage/InstructionPage.js
@@ -11,6 +11,8 @@ const InstructionPage = () => {
                     <span><hr size ={4} color='orange' className='secondInstruction'/></span>
                     <span> <hr size ={4} color='orange' className='firstInstruction'/> </span>
                 </div>
+
+                <p>Teaching students the material and guiding them through the learning process is the most  fundamental aspect of education.<br />We teaches a course or section of a course and includes sole-responsibility instructors</p>
             </div>
 
             
