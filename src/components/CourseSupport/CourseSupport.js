43c25e12022d3197240ff17948c9f6ff86aa8fcc
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-05 07:24:19 +0600

    base created

---
+++ src/components/CourseSupport/CourseSupport.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './CourseSupport.css';
+import { PiBookDuotone } from "react-icons/pi";
+
+const CourseSupport = () => {
+    return (
+        <div className='courseSupport'>
+            <div className="supportSection">
+                <div className="courseIntegrated">
+                    <span> <PiBookDuotone /> </span>
+                    <h1>Course Integrated</h1>
+                    <p>Contacr Dewaty Feaert</p>
+                </div>
+                <div className="videoSupport">
+                <span> <PiBookDuotone /> </span>
+                    <h1>Course Integrated</h1>
+                    <p>Contacr Dewaty Feaert</p>
+
+                </div>
+                <div className="cleanSupport">
+                <span> <PiBookDuotone /> </span>
+                    <h1>Course Integrated</h1>
+                    <p>Contacr Dewaty Feaert</p>
+
+                </div>
+
+
+            </div>
+
+
+        </div>
+    );
+};
+
+export default CourseSupport;
+
