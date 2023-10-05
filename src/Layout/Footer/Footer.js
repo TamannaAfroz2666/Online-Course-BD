65edd489aebebeee236a8037b5208442bc035d5d
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-05 20:07:03 +0600

    add structure create

--- src/Layout/Footer/Footer.js
+++ src/Layout/Footer/Footer.js
@@ -6,6 +6,17 @@ const Footer = () => {
         <div className='footer'>
             <div className="footerHead">
                 <h1>Get Started and <br />Grow Your Career</h1>
+                <div className="groupAll">
+                    <div className="documentation">
+                        <a href="/documentation"> Documentation</a>
+                    </div>
+                    <div className="tutorial">
+                        <a href="/tutorial"> Tutorial Videos </a>
+                    </div>
+                    <div className="purchaseNow">
+                        <a href="/purchase">Purchase Now</a>
+                    </div>
+                </div>
 
             </div>
             
